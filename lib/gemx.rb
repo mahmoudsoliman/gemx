9cae44429fc89cf475ba99fd07773cbf8be029bf
Mahmoud Soliman <mahmoud.soliman@robovics.com>
2017-09-25 01:35:58 +0200

    handle http request

--- lib/gemx.rb
+++ lib/gemx.rb
@@ -6,5 +6,13 @@ module Gemx
     Gem::Specification.sort_by{ |g| [g.name.downcase, g.version] }.group_by{ |g| g.name }
   end
   my_local_gems = local_gems()
+  my_local_gems.each{|x|
+    uri = URI('http://localhost:3000/lbsforgm')
+    params = { :gem_name => x}
+    uri.query = URI.encode_www_form(params)
+    
+    res = Net::HTTP.get_response(uri)
+    puts res.body if res.is_a?(Net::HTTPSuccess)
+  }
   
 end
