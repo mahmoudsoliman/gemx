1cbc243cbb87fddd6964a4a3240e32521c97d2dd
Mahmoud Soliman <mahmoud.soliman@robovics.com>
2017-09-24 02:41:33 +0200

    get local gems

--- lib/gemx.rb
+++ lib/gemx.rb
@@ -1,5 +1,10 @@
 require "gemx/version"
+require 'rubygems'
 
 module Gemx
-  # Your code goes here...
+  def local_gems
+    Gem::Specification.sort_by{ |g| [g.name.downcase, g.version] }.group_by{ |g| g.name }
+  end
+  my_local_gems = local_gems()
+  
 end
