35c3f3ebcd122ce72f2bd7505040e529e1c7614e
Mahmoud Soliman <mahmoud.soliman@robovics.com>
2017-09-25 22:03:36 +0200

    test_api

--- lib/gemx.rb
+++ lib/gemx.rb
@@ -1,18 +1,30 @@
 require "gemx/version"
+require 'open-uri'
+require 'net/http'
 require 'rubygems'
 
 module Gemx
-  def local_gems
+  def self.local_gems
     Gem::Specification.sort_by{ |g| [g.name.downcase, g.version] }.group_by{ |g| g.name }
   end
-  my_local_gems = local_gems()
-  my_local_gems.each{|x|
+
+  def self.get_lbs_list
+    my_local_gems = local_gems
+    my_local_gems.each{|x|
+      uri = URI('http://localhost:3000/lbsforgm')
+      params = { :gem_name => x}
+      uri.query = URI.encode_www_form(params)
+      
+      res = Net::HTTP.get_response(uri)
+      puts res.body #if res.is_a?(Net::HTTPSuccess)
+    }
+  end
+  def self.test_api
     uri = URI('http://localhost:3000/lbsforgm')
-    params = { :gem_name => x}
+    params = { :gem_name => 'nokogiri'}
     uri.query = URI.encode_www_form(params)
     
     res = Net::HTTP.get_response(uri)
-    puts res.body if res.is_a?(Net::HTTPSuccess)
-  }
-  
+    puts res.body #if res.is_a?(Net::HTTPSuccess)
+  end
 end
